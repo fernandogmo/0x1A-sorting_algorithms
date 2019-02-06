62370f98a723d01945620bb8b1e48eb5db37c5c5
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-02-05 19:59:54 -0800

    Added quicksort comments and removed partition from sort.h

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,17 @@
 #include "sort.h"
 
+/**
+ * swap - swaps to values in an int array if they are not equal, and
+ *        prints the current state of the array it was first called with.
+ * @a: first value to swap
+ * @b: second value to swap
+ * @array: pointer to first element of initial array
+ * @size: full size of initial array
+ */
 void swap(int *a, int *b, int *array, int size)
 {
+	static int *ptr, s; /* for printing */
 	int t = *a;
-	static int s = 0, *ptr = NULL; /* for printing  */
 
 	if (*a != *b)
 	{
@@ -15,6 +23,13 @@ void swap(int *a, int *b, int *array, int size)
 	}
 }
 
+/**
+ * partition - implements Lomuto partition scheme for an int array
+ * @array: int array to partition
+ * @lo: low index
+ * @hi: high index for pivot
+ * Return: index of partition
+ */
 int partition(int *array, int lo, int hi)
 {
 	int i, j, pivot = array[hi];
@@ -26,6 +41,11 @@ int partition(int *array, int lo, int hi)
 	return (i);
 }
 
+/**
+ * quick_sort - sorts an int array using partition-exchange sorting
+ * @array: int array to be sorted
+ * @size: length of @array
+ */
 void quick_sort(int *array, size_t size)
 {
 	if (size > 2)
