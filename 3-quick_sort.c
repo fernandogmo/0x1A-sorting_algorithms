17f4ced6ff4ba320dcb50ee63f52e19ae170073c
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-02-05 19:13:22 -0800

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,41 +1,43 @@
 #include "sort.h"
 
+void swap(int *a, int *b, int *array, int size)
+{
+	int t = *a;
+	static int count = 0, s = 0, *ptr = NULL; /* for printing  */
+
+	if (!ptr)
+		ptr = array, s = size;
+	*a = *b;
+	*b = t;
+	printf("%d: ", ++count);
+	print_array(ptr, s);
+}
+
 int partition(int *array, int lo, int hi)
 {
-	int t, j, i, pivot;
+	int i, j, pivot = array[hi];
 
-	i = lo;
-	printf("lo = %d, hi = %d\n", lo, hi);
-	if (lo > hi) exit(1);
-       	pivot = array[hi];
-	for (j = lo; j <= hi - 1; j++)
-	{
-		printf("array[j] = %d, pivot = %d\n", array[j], pivot);
+	for (j = i = lo; j < hi; j++)
 		if (array[j] <= pivot)
-		{
-			t = array[i];
-			array[i] = array[j];
-			array[j] = t;
-			print_array(array, hi + 1);
-			i++;
-		}
-	}
-	t = array[i];
-	array[i] = array[hi];
-	array[hi] = t;
+			swap(&array[i++], &array[j], array, hi + 1);
+	if (1)
+		swap(&array[i], &array[hi], array, hi + 1), puts("^outer");
 	return (i);
 }
 
-void q_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
-	int pivot = partition(array, 0, size - 1);
+	if (size > 2)
+	{
+		int pivot = partition(array, 0, size - 1);
 
-	q_sort(array, pivot);
-	q_sort(array + pivot + 1, size - pivot - 1);
+		quick_sort(array, pivot);
+		quick_sort(array + pivot + 1, size - pivot - 1);
+	}
 }
 
 
-void quick_sort(int *array, size_t size)
+/*void quick_sort(int *array, size_t size)
 {
 	int i, j;
 	int pivot = array[size / 2];
@@ -60,4 +62,4 @@ void quick_sort(int *array, size_t size)
 	}
 	quick_sort(array, i);
 	quick_sort(array + i, size - i);
-}
+}*/
