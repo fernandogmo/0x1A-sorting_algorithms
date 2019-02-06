de750ce680ac8a316de65facdd0fd80b782ab016
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-02-05 19:41:10 -0800

    It works! Still missing function comments though...

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,14 +3,16 @@
 void swap(int *a, int *b, int *array, int size)
 {
 	int t = *a;
-	static int count = 0, s = 0, *ptr = NULL; /* for printing  */
+	static int s = 0, *ptr = NULL; /* for printing  */
 
-	if (!ptr)
-		ptr = array, s = size;
-	*a = *b;
-	*b = t;
-	printf("%d: ", ++count);
-	print_array(ptr, s);
+	if (*a != *b)
+	{
+		*a = *b;
+		*b = t;
+		if (!ptr)
+			ptr = array, s = size;
+		print_array(ptr, s);
+	}
 }
 
 int partition(int *array, int lo, int hi)
@@ -18,10 +20,9 @@ int partition(int *array, int lo, int hi)
 	int i, j, pivot = array[hi];
 
 	for (j = i = lo; j < hi; j++)
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 			swap(&array[i++], &array[j], array, hi + 1);
-	if (1)
-		swap(&array[i], &array[hi], array, hi + 1), puts("^outer");
+	swap(&array[i], &array[hi], array, hi + 1);
 	return (i);
 }
 
@@ -35,31 +36,3 @@ void quick_sort(int *array, size_t size)
 		quick_sort(array + pivot + 1, size - pivot - 1);
 	}
 }
-
-
-/*void quick_sort(int *array, size_t size)
-{
-	int i, j;
-	int pivot = array[size / 2];
-
-	if (size < 2)
-		return;
-	for (i = 0, j = size - 1; ; i++, j--)
-	{
-		while (array[i] < pivot)
-			i++;
-		while (array[j] > pivot)
-			j--;
-		if (i < j)
-		{
-			int temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array(array, size);
-		}
-		else
-			break;
-	}
-	quick_sort(array, i);
-	quick_sort(array + i, size - i);
-}*/
