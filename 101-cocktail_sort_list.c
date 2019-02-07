18caef01fcde24caff1029c1cbe236f9bf7a49ba
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-02-06 20:45:41 -0800

    Change flag names. That's it

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,8 +7,8 @@
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *temp = *list;
-	int flag = 0;
-	int flag2 = 0;
+	int swapped = 0;
+	int phase = 0;
 
 	while (temp)
 	{
@@ -24,24 +24,24 @@ void cocktail_sort_list(listint_t **list)
 				temp->prev->next = temp;
 			else
 				*list = temp;
-			flag = 1;
+			swapped = 1;
 			print_list(*list);
 		}
-		if (flag2 == 0 && temp->next)
-			temp = temp->next;
-		else if (flag2 == 1 && temp->prev)
+		if (phase == 0 && temp->next)
+			temp = temp->next, printf("temp = %d\n", temp->n);
+		else if (phase == 1 && temp->prev)
 			temp = temp->prev;
-		if (flag == 0 && (temp->next == NULL || temp->prev == NULL))
+		if (swapped == 0 && (temp->next == NULL || temp->prev == NULL))
 			return;
-		else if (flag == 1 && temp->next == NULL)
+		else if (swapped == 1 && temp->next == NULL)
 		{
-			flag = 0;
-			flag2 = 1;
+			swapped = 0;
+			phase = 1;
 		}
-		else if (flag == 1 && temp->prev == NULL)
+		else if (swapped == 1 && temp->prev == NULL)
 		{
-			flag = 0;
-			flag2 = 0;
+			swapped = 0;
+			phase = 0;
 		}
 	}
 }
