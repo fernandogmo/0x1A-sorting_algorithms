a886577ff575407276e9011d4f3415a268da83cb
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-02-07 23:19:53 -0800

    Added betty comments

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,13 +1,19 @@
 #include "sort.h"
 
-
+/**
+ * merge - Merges left and right subarrays using temp buffer.
+ * @array: int array of integers.
+ * @size: size_t length of array.
+ * @m: size_t midpoint of array.
+ */
 void merge(int *array, size_t size, size_t m)
 {
 	size_t i = 0, j = m, k = 0;
 	int *tmp = malloc(size * sizeof(*array));
 
 	puts("Merging...");
-	printf("[left]: "), print_array(array, size - m);
+	printf("[left]: "), print_array(array, m);
+	printf("[right]: "), print_array(array + m, size - m);
 	while (k < size)
 		if (j == size)
 			tmp[k++] = array[i++];
@@ -25,6 +31,12 @@ void merge(int *array, size_t size, size_t m)
 	free(tmp);
 }
 
+/**
+ * merge_sort - Sort an array of integers in ascending order
+ *              using a top-down merge sort algorithm.
+ * @array: int array of integers.
+ * @size: size_t length of array.
+ */
 void merge_sort(int *array, size_t size)
 {
 	if (size > 1)
